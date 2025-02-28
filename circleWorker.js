a52b2d738d6364cb02e9f0811f82da17662c7634
Luiz Ramos <luiz.matthew187@gmail.com>
2025-02-27 16:26:23 -0800

    General performance improvements

--- circleWorker.js
+++ circleWorker.js
@@ -1,117 +1,178 @@
-// circleWorker.js
-importScripts(
-    'https://cdnjs.cloudflare.com/ajax/libs/Turf.js/6.5.0/turf.min.js',
-    'https://cdn.jsdelivr.net/npm/rbush@4.0.1/rbush.min.js'
-  );
-  
-  self.onmessage = function(event) {
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/Turf.js/6.5.0/turf.min.js','https://cdn.jsdelivr.net/npm/rbush@4.0.1/rbush.min.js');
+
+self.onmessage = function (event) {
+    
     const { circleCoordinates, dotCoordinates } = event.data;
-  
-    // Sets to store indices to hide
+
     const hiddenCircleIndices = new Set();
     const hiddenDotIndices = new Set();
-  
-    // Helper: approximate search box delta for 5 miles.
-    // 1 degree latitude is roughly 69 miles.
-    const milesToLatDegrees = miles => miles / 69;
-  
-    // Build a spatial index for circleCoordinates
-    const circleTree = new RBush();
-    circleCoordinates.forEach((pt, i) => {
-      const lat = pt.lat;
-      const lon = pt.lon;
-      const deltaLat = milesToLatDegrees(5);
-      // For longitude, adjust by cosine of the latitude (in radians)
-      const deltaLon = 5 / (69 * Math.cos(lat * Math.PI / 180));
-      circleTree.insert({
-        minX: lon - deltaLon,
-        minY: lat - deltaLat,
-        maxX: lon + deltaLon,
-        maxY: lat + deltaLat,
-        id: i
-      });
+// 
+    // Perform distance calculations to hide circles within 5 miles from eachother
+    const PstartTime = performance.now()
+ 
+
+
+     ///////////////////////////////////////Rank1/////////////////////////////
+    const tree = new RBush();
+
+    // // Insert all circles into RBush
+    circleCoordinates.forEach((circle, index) => {
+        const bbox = turf.bbox(turf.point([circle.lon, circle.lat])); // Get bounding box
+        tree.insert({ minX: bbox[0], minY: bbox[1], maxX: bbox[2], maxY: bbox[3], id: index });
     });
-  
-    // For each circle point, query the spatial index for neighbors within the search box.
-    circleCoordinates.forEach((pt, i) => {
-      if (hiddenCircleIndices.has(i)) return;
-  
-      const lat = pt.lat;
-      const lon = pt.lon;
-      const deltaLat = milesToLatDegrees(5);
-      const deltaLon = 5 / (69 * Math.cos(lat * Math.PI / 180));
-      const searchBBox = {
-        minX: lon - deltaLon,
-        minY: lat - deltaLat,
-        maxX: lon + deltaLon,
-        maxY: lat + deltaLat
-      };
-  
-      const neighbors = circleTree.search(searchBBox);
-      neighbors.forEach(neighbor => {
-        if (neighbor.id === i) return; // Skip self
-        if (hiddenCircleIndices.has(neighbor.id)) return;
-        // Only process each pair once
-        if (neighbor.id > i) {
-          const from = turf.point([lon, lat]);
-          const to = turf.point([circleCoordinates[neighbor.id].lon, circleCoordinates[neighbor.id].lat]);
-          const distance = turf.distance(from, to, { units: 'miles' });
-          if (distance <= 5) {
-            hiddenCircleIndices.add(neighbor.id);
-          }
-        }
-      });
+    circleCoordinates.forEach((circle, index) => {
+        if (hiddenCircleIndices.has(index)) return;
+    
+        // Get the bounding box of the circle
+        const bbox = turf.bbox(turf.buffer(turf.point([circle.lon, circle.lat]), 5, { units: 'miles' }));
+    
+        // Query RBush for nearby circles
+        const nearbyCircles = tree.search({ minX: bbox[0], minY: bbox[1], maxX: bbox[2], maxY: bbox[3] });
+    
+        nearbyCircles.forEach((neighbor) => {
+            if (neighbor.id !== index && !hiddenCircleIndices.has(neighbor.id)) {
+                const from = turf.point([circle.lon, circle.lat]);
+                const to = turf.point([circleCoordinates[neighbor.id].lon, circleCoordinates[neighbor.id].lat]);
+                const distance = turf.distance(from, to, { units: 'miles' });
+    
+                if (distance <= 5) {
+                    hiddenCircleIndices.add(neighbor.id);
+                }
+            }
+        });
     });
-  
-    // Repeat a similar process for dotCoordinates (which use lon2/lat2)
-    const dotTree = new RBush();
-    dotCoordinates.forEach((pt, i) => {
-      const lat = pt.lat2;
-      const lon = pt.lon2;
-      const deltaLat = milesToLatDegrees(5);
-      const deltaLon = 5 / (69 * Math.cos(lat * Math.PI / 180));
-      dotTree.insert({
-        minX: lon - deltaLon,
-        minY: lat - deltaLat,
-        maxX: lon + deltaLon,
-        maxY: lat + deltaLat,
-        id: i
-      });
-    });
-  
-    dotCoordinates.forEach((pt, i) => {
-      if (hiddenDotIndices.has(i)) return;
-  
-      const lat = pt.lat2;
-      const lon = pt.lon2;
-      const deltaLat = milesToLatDegrees(5);
-      const deltaLon = 5 / (69 * Math.cos(lat * Math.PI / 180));
-      const searchBBox = {
-        minX: lon - deltaLon,
-        minY: lat - deltaLat,
-        maxX: lon + deltaLon,
-        maxY: lat + deltaLat
-      };
-  
-      const neighbors = dotTree.search(searchBBox);
-      neighbors.forEach(neighbor => {
-        if (neighbor.id === i) return;
-        if (hiddenDotIndices.has(neighbor.id)) return;
-        if (neighbor.id > i) {
-          const from = turf.point([lon, lat]);
-          const to = turf.point([dotCoordinates[neighbor.id].lon2, dotCoordinates[neighbor.id].lat2]);
-          const distance = turf.distance(from, to, { units: 'miles' });
-          if (distance <= 5) {
-            hiddenDotIndices.add(neighbor.id);
-          }
-        }
-      });
-    });
-  
+
+
+
+   
+    // /////////////////////Rank 2/////////////////////////////////////
+    // console.log("took circle length",circleCoordinates.length);
+    // circleCoordinates.forEach((circleA, indexA) => {
+    //     for (let indexB = indexA + 1; indexB < circleCoordinates.length; indexB++) {
+    //         const circleB = circleCoordinates[indexB];
+    
+    //         const from = turf.point([circleA.lon, circleA.lat]);
+    //         const to = turf.point([circleB.lon, circleB.lat]);
+    //         const distance = turf.distance(from, to, { units: 'miles' });
+    
+    //         if (distance <= 5) {
+    //             hiddenCircleIndices.add(indexB);
+    //         }
+    //     }
+    // });
+
+
+   
+     //////////////////////////////OLD CODE////////////////////////////////////   
+    
+    // circleCoordinates.forEach((circleA, indexA) => {
+    //     if (hiddenCircleIndices.has(indexA)) return;
+
+    //     circleCoordinates.forEach((circleB, indexB) => {
+    //         if (indexA === indexB || hiddenCircleIndices.has(indexB)) return;
+
+    //         const from = turf.point([circleA.lon, circleA.lat]);
+    //         const to = turf.point([circleB.lon, circleB.lat]);
+    //         const distance = turf.distance(from, to, { units: 'miles' });
+
+    //         if (distance <= 5) {
+    //             hiddenCircleIndices.add(indexB);
+    //             console.log("distance", distance, indexB, indexA, circleA.lon, circleA.lat, circleB.lon, circleB.lat)
+    //         }
+    //     });
+    // });
+
+    const PendTime = performance.now();
+    const executionTime = PendTime - PstartTime;
+    console.log(`circleCoordinates.forEach in CircleWorker took ${executionTime} milliseconds to execute.`);
+
+    
+
+    
+    //////////////////////////////
+     const MstartTime = performance.now()
+    console.log("took dot length",dotCoordinates.length);
+     /////////////////////////Rank1 ///////////////////////////////////
+     
+
+    //  // Insert all dots into RBush for fast spatial lookups
+         dotCoordinates.forEach((dot, index) => {
+             const bbox = turf.bbox(turf.point([dot.lon2, dot.lat2]));
+             tree.insert({ minX: bbox[0], minY: bbox[1], maxX: bbox[2], maxY: bbox[3], id: index });
+         });
+     
+     // Use a Typed Array for fast lookups instead of Set()
+         
+     
+     // Array to store logs for batch logging
+         const logMessages = [];
+     
+     // Optimized loop: Query only nearby dots using RBush
+         dotCoordinates.forEach((dot, index) => {
+         if (hiddenDotIndices[index]) return;
+     
+         // Get nearby dots (~5 miles radius)
+         const bbox = turf.bbox(turf.buffer(turf.point([dot.lon2, dot.lat2]), 5, { units: 'miles' }));
+         const nearbyDots = tree.search({ minX: bbox[0], minY: bbox[1], maxX: bbox[2], maxY: bbox[3] });
+     
+         nearbyDots.forEach((neighbor) => {
+             if (neighbor.id !== index && !hiddenDotIndices[neighbor.id]) {
+                 const from = turf.point([dot.lon2, dot.lat2]);
+                 const to = turf.point([dotCoordinates[neighbor.id].lon2, dotCoordinates[neighbor.id].lat2]);
+                 const distance = turf.distance(from, to, { units: 'miles' });
+     
+                 if (distance <= 5) {
+                     hiddenDotIndices[neighbor.id] = 1;
+                     logMessages.push(`Hiding dot ${neighbor.id} (too close to ${index})`);
+                 }
+             }
+         });
+     });
+    ////////////////////////////Rank2/////////////////////////////////////////
+   
+    // dotCoordinates.forEach((circleA, indexA) => {
+    //     if (hiddenDotIndices.has(indexA)) return;
+    
+    //     for (let indexB = indexA + 1; indexB < dotCoordinates.length; indexB++) {
+    //         if (hiddenDotIndices.has(indexB)) continue;
+    
+    //         const circleB = dotCoordinates[indexB];
+    
+    //         const from = turf.point([circleA.lon2, circleA.lat2]);
+    //         const to = turf.point([circleB.lon2, circleB.lat2]);
+    //         const distance = turf.distance(from, to, { units: 'miles' });
+    
+    //         if (distance <= 5) {
+    //             hiddenDotIndices.add(indexB);
+    //         }
+    //     }
+    // });
+
+    /////////////////////////OLD CODE///////////////////////////////////////////////////
+    // dotCoordinates.forEach((circleA, indexA) => {
+    //     if (hiddenDotIndices.has(indexA)) return;
+
+    //     dotCoordinates.forEach((circleB, indexB) => {
+    //         if (indexA === indexB || hiddenDotIndices.has(indexB)) return;
+
+    //         const from = turf.point([circleA.lon2, circleA.lat2]);
+    //         const to = turf.point([circleB.lon2, circleB.lat2]);
+    //         const distance = turf.distance(from, to, { units: 'miles' });
+
+    //         if (distance <= 5) {
+    //             hiddenDotIndices.add(indexB);
+    //         }
+    //     });
+    // });
+    const MendTime = performance.now()
+    const MexecutionTime= MendTime - MstartTime
+    console.log(`dotCoordinates.forEach in CircleWorker took ${MexecutionTime} milliseconds to execute.`);
+
+
     // Send results back to the main thread
     self.postMessage({
-      hiddenCircleIndices: Array.from(hiddenCircleIndices),
-      hiddenDotIndices: Array.from(hiddenDotIndices)
+        hiddenCircleIndices: Array.from(hiddenCircleIndices),
+        hiddenDotIndices: Array.from(hiddenDotIndices),
     });
-  };
-  
+};
+
